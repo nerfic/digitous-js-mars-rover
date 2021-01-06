c951b4704713a2583d967a53affc42a2d81bc4f4
Nerfic <jdll.allan@gmail.com>
2021-01-06 10:59:46 +0100

    add func

--- projet.js
+++ projet.js
@@ -1,5 +1,5 @@
 var grid = [
-    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    ["R", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -10,3 +10,30 @@ var grid = [
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
+
+var rover = {
+    direction: "N",
+    x: 0,
+    y: 0
+}
+
+function turnLeft(rover) {
+
+}
+
+function turnRight(rover) {
+
+}
+
+console.log("Départ du rover \n", grid.join('\n') + '\n\n');
+
+// newposition = position ancienne
+// poisition ancienne = ' '
+
+grid[0][1] = grid[0][0]
+grid[0][0] = ' '
+console.log("Position suivante\n", grid.join('\n'))
+
+grid[1][1] = grid[0][1]
+grid[0][1] = ' '
+console.log("Position suivante\n", grid.join('\n'))
